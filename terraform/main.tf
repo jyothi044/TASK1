c88de949b9ac5fbf8f993ac8470dc151897538d1
jyothi044 <jyothi.bmru18@gmail.com>
2025-08-20 15:48:20 +0530

    fixed the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,30 +3,9 @@ data "aws_vpc" "selected" {
   id = "vpc-0170241f47915e239"
 }
 
-# Create new Security Group to allow all IPs for PostgreSQL
-resource "aws_security_group" "rds_sg" {
-  name        = "rds-postgres-allow-all"
-  description = "Allow all inbound traffic to PostgreSQL port 5432"
-  vpc_id      = data.aws_vpc.selected.id
-
-  ingress {
-    description = "Allow PostgreSQL from all IPs"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "rds-postgres-allow-all"
-  }
+# Use existing Security Group
+data "aws_security_group" "rds_sg" {
+  id = "sg-0a5e5063632dd3715"
 }
 
 resource "aws_subnet" "public_subnet_1" {
@@ -71,6 +50,6 @@ resource "aws_db_instance" "postgres" {
   db_name                = var.db_name
   publicly_accessible    = true
   skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.public_subnet_group.name
 }
