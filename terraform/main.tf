f2ace436c7d34976337aeea050af4204de2b37e5
jyothi044 <jyothi.bmru18@gmail.com>
2025-08-20 15:11:20 +0530

    fixed the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,22 +8,34 @@ data "aws_security_group" "rds_sg" {
   id = "sg-053c9577e60545b6d"
 }
 
-# Public Subnet (in eu-north-1b)
-resource "aws_subnet" "public_subnet" {
+# Public Subnet 1 (in eu-north-1b)
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = data.aws_vpc.selected.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-north-1b"
 
   tags = {
-    Name = "fastapi-public-subnet"
+    Name = "fastapi-public-subnet-1"
   }
 }
 
-# Subnet group for RDS (single subnet, note this may not meet AZ coverage requirement)
+# Public Subnet 2 (in eu-north-1a for AZ coverage)
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = data.aws_vpc.selected.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-north-1c"
+
+  tags = {
+    Name = "fastapi-public-subnet-2"
+  }
+}
+
+# Subnet group for RDS (include both subnets for AZ coverage)
 resource "aws_db_subnet_group" "public_subnet_group" {
   name       = "public-subnet-group"
-  subnet_ids = [aws_subnet.public_subnet.id]
+  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 
   tags = {
     Name = "public-subnet-group"
