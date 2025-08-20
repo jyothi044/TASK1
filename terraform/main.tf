acd273b53afb83f4d4fbb2c1d7a66b010e4590ff
jyothi044 <jyothi.bmru18@gmail.com>
2025-08-20 15:42:47 +0530

    fixed the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,37 @@ data "aws_vpc" "selected" {
   id = "vpc-0170241f47915e239"
 }
 
-# Use existing Security Group
-data "aws_security_group" "rds_sg" {
-  id = "sg-053c9577e60545b6d"
+# Create new Security Group to allow all IPs for PostgreSQL
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-postgres-allow-all"
+  description = "Allow all inbound traffic to PostgreSQL port 5432"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    description = "Allow PostgreSQL from all IPs"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-postgres-allow-all"
+  }
 }
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = data.aws_vpc.selected.id
-  cidr_block              = "0.0.0.0/0"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1d"
+  availability_zone       = "eu-north-1a"
 
   tags = {
     Name = "fastapi-public-subnet-1"
@@ -21,7 +42,7 @@ resource "aws_subnet" "public_subnet_1" {
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = data.aws_vpc.selected.id
-  cidr_block              = "0.0.0.0/0"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-north-1c"
 
@@ -50,6 +71,6 @@ resource "aws_db_instance" "postgres" {
   db_name                = var.db_name
   publicly_accessible    = true
   skip_final_snapshot    = true
-  vpc_security_group_ids = [data.aws_security_group.rds_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.public_subnet_group.name
 }
