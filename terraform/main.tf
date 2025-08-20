8ced6d0b5e36154acafbeeceab555bfe80a41bd8
jyothi044 <jyothi.bmru18@gmail.com>
2025-08-20 12:28:44 +0530

    fixed the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,74 +1,27 @@
+# --- Get Default VPC ---
+data "aws_vpc" "default" {
+  default = true
+}
 
-# --- VPC ---
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "fastapi-vpc"
+# --- Get Default Subnets ---
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
-}
-
-# --- Subnets ---
-resource "aws_subnet" "public_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "fastapi-subnet-a"
-  }
-}
-
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-north-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "fastapi-subnet-b"
-  }
-}
-
-# --- Internet Gateway ---
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "fastapi-igw"
-  }
-}
-
-# --- Route Table ---
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-# --- Route Table Associations ---
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public.id
 }
 
 # --- Security Group ---
 resource "aws_security_group" "postgres_sg" {
   name        = "postgres-sg"
   description = "Allow Postgres inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # WARNING: open to all, for demo only
   }
 
   egress {
@@ -82,7 +35,7 @@ resource "aws_security_group" "postgres_sg" {
 # --- DB Subnet Group ---
 resource "aws_db_subnet_group" "default" {
   name       = "postgres-subnet-group"
-  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  subnet_ids = data.aws_subnets.default.ids
   tags = {
     Name = "postgres-subnet-group"
   }
@@ -97,7 +50,7 @@ resource "aws_db_instance" "postgres" {
   instance_class         = "db.t3.micro"
   username               = "postgres"
   password               = "password123"
-  parameter_group_name   = "default.postgres15"
+  parameter_group_name   = "default.postgres16"
   skip_final_snapshot    = true
   publicly_accessible    = true
 
