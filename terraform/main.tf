dd9766dad6b67cd5a0eb8f82c70b1bd713bd242c
jyothi044 <jyothi.bmru18@gmail.com>
2025-08-20 15:09:17 +0530

    fixed the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,65 +8,22 @@ data "aws_security_group" "rds_sg" {
   id = "sg-053c9577e60545b6d"
 }
 
-# Use existing Internet Gateway (specific ID from screenshot)
-data "aws_internet_gateway" "existing_igw" {
-  id = "igw-08d5673bd0ea8f6e4"
-}
-
-# Public Subnet 1 (in eu-north-1b)
-resource "aws_subnet" "public_subnet_1" {
+# Public Subnet (in eu-north-1b)
+resource "aws_subnet" "public_subnet" {
   vpc_id                  = data.aws_vpc.selected.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-north-1b"
 
   tags = {
-    Name = "fastapi-public-subnet-1"
+    Name = "fastapi-public-subnet"
   }
 }
 
-# Public Subnet 2 (in eu-north-1a for AZ coverage)
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = data.aws_vpc.selected.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1c"
-
-  tags = {
-    Name = "fastapi-public-subnet-2"
-  }
-}
-
-# Route Table (using existing Internet Gateway)
-resource "aws_route_table" "public_rt" {
-  vpc_id = data.aws_vpc.selected.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.existing_igw.id
-  }
-
-  tags = {
-    Name = "fastapi-public-rt"
-  }
-}
-
-# Route Table Association for Subnet 1
-resource "aws_route_table_association" "public_assoc_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# Route Table Association for Subnet 2
-resource "aws_route_table_association" "public_assoc_2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# Subnet group for RDS (include both subnets for AZ coverage)
+# Subnet group for RDS (single subnet, note this may not meet AZ coverage requirement)
 resource "aws_db_subnet_group" "public_subnet_group" {
   name       = "public-subnet-group"
-  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  subnet_ids = [aws_subnet.public_subnet.id]
 
   tags = {
     Name = "public-subnet-group"
